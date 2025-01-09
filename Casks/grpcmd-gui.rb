bcfc2b1887719048d4606d104c3017870ff4784f
Akshay Kalose <akshay@kalose.net>
2025-01-09 21:44:24 +0530

    Update grpcmd-gui.rb
    Update homepage to GitHub repo.

--- Casks/grpcmd-gui.rb
+++ Casks/grpcmd-gui.rb
@@ -5,11 +5,10 @@ cask "grpcmd-gui" do
   sha256 arm:   "f25e94f2c48d41667dcffc09cf96eed1f49a32ecad7b96292224ccdfcd464a67",
          intel: "9db44e406e380bed90b2a3bc133897c32a685cf42e918b355b8ca58513f15dde"
 
-  url "https://github.com/grpcmd/grpcmd-gui/releases/download/v#{version}/grpcmd-gui_v#{version}_darwin-#{arch}-installer.dmg",
-      verified: "github.com/grpcmd/grpcmd-gui/"
+  url "https://github.com/grpcmd/grpcmd-gui/releases/download/v#{version}/grpcmd-gui_v#{version}_darwin-#{arch}-installer.dmg"
   name "grpcmd-gui"
   desc "gRPC GUI"
-  homepage "https://grpc.md"
+  homepage "https://github.com/grpcmd/grpcmd-gui"
 
   depends_on macos: ">= :catalina"
 
