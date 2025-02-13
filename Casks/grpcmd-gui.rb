32161c599da3c490ed7c5984cd601532aed3cd70
Akshay Kalose <akshay@kalose.net>
2025-02-13 20:38:03 +0530

    grpcmd-gui 0.3.0 (#2)

--- Casks/grpcmd-gui.rb
+++ Casks/grpcmd-gui.rb
@@ -1,9 +1,9 @@
 cask "grpcmd-gui" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.2.0"
-  sha256 arm:   "adc98ed3e2f1395b1575c63dfd0b4527b1a21c4d2442e6290b9f72d2d4e6aee2",
-         intel: "957d424edfd661c82515f81d7e99bba28f02e24040bb26e57737c036cc78c583"
+  version "0.3.0"
+  sha256 arm:   "c94ac7cae8d804f0b3305dfb08bef4c4fe9857a2318d70f635cf5c476165cbec",
+         intel: "6b1eecebfe23cc10b52ae53abdee0b5847d628571487c0ccc67fef4bf02ad164"
 
   url "https://github.com/grpcmd/grpcmd-gui/releases/download/v#{version}/grpcmd-gui_v#{version}_darwin-#{arch}-installer.dmg"
   name "grpcmd-gui"
